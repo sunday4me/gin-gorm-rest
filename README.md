829a8846cd38db9424010dcc5d1f024ab83e64f2
Sunday Jimoh <sunday4me@gmail.com>
2023-03-07 23:59:52 +0100

    adding the rest of the code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # gin-gorm-rest
+Go REST API With GIN & GORM | Postgres | Docker
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main() {
 router := gin.New()
-router.Get("/", func(c *gin.Context){
-c.String(200, "Hello World!")
-})
+
+router.GET("/", func(c *gin.Context){
+	c.String(200, "Hello World!")	
+	})
+
 router.Run(":8080")
-}
+			}
